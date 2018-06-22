1a65125c4b4e10e49bf1a5477337584072b85f97
untitleX <40496773+untitleX@users.noreply.github.com>
2018-06-22 12:57:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-print(' welcome to hiyacfw installer (some name) ')
-print(' v1.0')
-print(' this version is ealry in developemt. Proceed with caution')
+import time
+#initial messages
+print('Welcome to hiyacfw helper (some name) v1.0')
+time.sleep(0.5)
+print('This version is early in developemt.')
+time.sleep(0.5)
+print('Proceed with caution')
+time.sleep(0.5)
 
 while True:
   
   ans = input('Are you going to use the default downloads folder for your downloads? [Y/N]').lower()
   if ans in ['y', 'yes', 'yep', 'yeah why not', 'alright', 'fine']:
+    dwndir = '%USERPROFILE%\Downloads\'
     break
   elif ans in ['n', 'no', 'nope', 'no way', 'not a chance', 'not happening']:
     print(' please enter the directory of the folder.')
     dwndir = 'C:\\' + input('Downloads directory = C:\\')
-    # %USERPROFILE%\Downloads\ by default
     break
+
+checkA = os.path.exists(dwndir + 'file_needed.zip')
+checkB = os.path.isfile(dwndir + 'file_needed.zip')
+if checkA and checkB:
+  pass
+  #continue with program
+else:
+  print('file ' + 'file_needed.zip' + 'not found')
