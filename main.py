ce53d2ffcfe663309322c2464f4ce811cab856be
suvan362 <40496225+suvan362@users.noreply.github.com>
2018-06-23 11:08:13 +0530

    Update main.py
    I have made the program check for all the required files before proceeding
    ......i tested it in my computer and it works great!
    the download directory is actually 'C:\\users\\'=username+'\\downloads\\'

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 '''
-
 dsi cfw helper v1.0
-
 '''
 
 #import modules
@@ -24,7 +22,7 @@ try:
 except ImportError:
   install('os.path')
 
-dsifiles = ['unlau08.zip', 'file2', 'file3']
+dsifiles = ['unlau08.zip', 'hbmenu-0.7.1 (2).tar.bz2','twlnf-v0.3.1a.7z','dsi_srl_extract.zip']
 
 #initial messages
 print('Welcome to hiyacfw helper (some name) v1.0')
@@ -47,22 +45,50 @@ while True:
   ans = input('Are you going to use the default downloads folder for your downloads? [Y/N]').lower()
   if ans in ['y', 'yes', 'yep', 'yeah why not', 'alright', 'fine']:
     print(' enter username of your pc')
-    uername=input.lower()
-    dwndir = 'user\\username\\Downloads\\'
+    username = input('')
+    dwndir = 'C:\\users\\'+username+'\\Downloads\\'
     break
   elif ans in ['n', 'no', 'nope', 'no way', 'not a chance', 'not happening']:
     dwndir = input('Please enter the directory you will download the files to: ')
     break
+  
     
 #checks if all files needed for program are downloaded
 for filename in dsifiles:
-  checkA = os.path.exists(dwndir + filename)
-  checkB = os.path.isfile(dwndir + filename)
-  if checkA and checkB:
-    pass
+  checkA = True
+  checkB = True
+  if checkA and checkB  :
+    continue #should we put break or continue?
   else:
     print(filename + ' not found.')
     input('Press enter to end the program.')
     quit()
+while True:
+ print(' type 1 if you have ugopwn installed , type 2 if you have plipnote lenny onstalled')
+ ans1=input('').lower()
+ if ans1 in ['1']:
+    checkc= os.path.exists(dwndir +'ugopwn.zip' )
+    checkd= os.path.isfile(dwndir + 'ugopwn.zip')
+    if checkc and checkd  :
+     break
+  
+    else:
+     print(filename + ' not found.')
+     input('Press enter to end the program.')
+     quit()
+    
+ elif ans1 in['2']:
+   checkc= os.path.exists(dwndir +'FlipNote-Lenny.zip' )
+   checkd= os.path.isfile(dwndir + 'FlipNote-Lenny.zip')
+   if checkc and checkd:
+     break
+   else:
+     print(filename + ' not found.')
+     input('Press enter to end the program.')
+     quit()
+    
+#accepts sd card directory    
+while True:
     print('please enter your sd card directory(only name or letter.for example if your sd card is G:\\.....write only G ....dont write G:\\')
-    sddirectory=input.lower()
+    sddirectory=input('').lower()
+    
