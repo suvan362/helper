1f88d423d0cf8418affdeba716acef87f8d0e3b8
suvan362 <40496225+suvan362@users.noreply.github.com>
2018-06-23 08:41:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,4 +63,4 @@ for filename in dsifiles:
     input('Press enter to end the program.')
     quit()
     print('please enter your sd card directory(only name or letter.for example if your sd card is G:\.....write only G ....dont write G:\')
-    
+    sddirectory=input.lower()
