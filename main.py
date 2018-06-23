990445807721aeb7db4f2d7711a2f68bbd1e307f
untitleXYZ <40496773+untitleXYZ@users.noreply.github.com>
2018-06-23 11:37:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -22,15 +22,12 @@ try:
 except ImportError:
   install('os.path')
 
-dsifiles = ['unlau08.zip', 'hbmenu-0.7.1 (2).tar.bz2','twlnf-v0.3.1a.7z','dsi_srl_extract.zip']
+dsifiles = ['unlau08.zip', 'hbmenu-0.7.1.tar.bz2','twlnf-v0.3.1a.7z','dsi_srl_extract.zip']
 
 #initial messages
 print('Welcome to hiyacfw helper (some name) v1.0')
-time.sleep(1)
 print('This version is early in developemt.')
-time.sleep(1)
 print('Proceed with caution.')
-time.sleep(1)
 input('Press enter to continue.')
 print('Please install all the packages mentioned below. They can all be found in the README.')
 print('1. Unlaunch v0.8. We are using 0.8 as 0.9 has bugs preventing HiyaCFW from working.')
@@ -45,13 +42,18 @@ while True:
   ans = input('Are you going to use the default downloads folder for your downloads? [Y/N]').lower()
   if ans in ['y', 'yes', 'yep', 'yeah why not', 'alright', 'fine']:
     print(' enter username of your pc')
-    username = input('')
-    dwndir = 'C:\\users\\'+username+'\\Downloads\\'
+    dwndir = '%USERPROFILE%\\Downloads\\'
     break
   elif ans in ['n', 'no', 'nope', 'no way', 'not a chance', 'not happening']:
-    dwndir = input('Please enter the directory you will download the files to: ')
+    print('Please enter the directory you will download the files to.')
+    dwndir = input('')
     break
-  
+    
+#establishes sd card drive    
+while True:
+    print('Please enter your SD Card drive letter. ex: `G`, `H`, `I`')
+    sddir = input('').lower()
+    #if is letter between A-Z not C then break
     
 #checks if all files needed for program are downloaded.
 for filename in dsifiles:
@@ -59,36 +61,35 @@ for filename in dsifiles:
    checkB = os.path.isfile(dwndir + filename)
    if checkA and checkB:
       pass
-  else:
-    print(filename + ' not found.')
-    input('Press enter to end the program.')
-    quit()
-while True:
- print(' type 1 if you have ugopwn installed , type 2 if you have flipnote lenny installed')
- ans1=input('').lower()
- if ans1 in ['1']:
-    checkc= os.path.exists(dwndir +'ugopwn.zip' )
-    checkd= os.path.isfile(dwndir + 'ugopwn.zip')
-    if checkc and checkd  :
-     break
-  
     else:
-     print( 'ugopwn not found.')
-     input('Press enter to end the program.')
+      print(filename + ' not found.')
+      input('Press enter to end the program. ')
+      quit()
+
+
+print('Enter 1 if you have ugopwn installed.')
+print('Enter 2 if you have flipnote lenny installed.')
+ans = input('')
+
+while True:
+  if ans == '1':
+    checkA = os.path.exists(dwndir + 'ugopwn.zip' )
+    checkB = os.path.isfile(dwndir + 'ugopwn.zip')
+    if checkA and checkB:
+      break
+    else:
+     print('ugopwn not found.')
+     input('Press enter to end the program. ')
      quit()
-    
- elif ans1 in['2']:
-   checkc= os.path.exists(dwndir +'FlipNote-Lenny.zip' )
-   checkd= os.path.isfile(dwndir + 'FlipNote-Lenny.zip')
-   if checkc and checkd:
+
+  elif ans == '2':
+   checkA = os.path.exists(dwndir + 'FlipNote-Lenny.zip')
+   checkB = os.path.isfile(dwndir + 'FlipNote-Lenny.zip')
+   if checkA and checkB:
      break
    else:
-     print( 'flipnote lenny not found.')
-     input('Press enter to end the program.')
+     print('flipnote lenny not found.')
+     input('Press enter to end the program. ')
      quit()
-    
-#accepts sd card directory    
-while True:
-    print('please enter your sd card directory(only name or letter.for example if your sd card is G:\\.....write only G ....dont write G:\\')
-    sddirectory=input('').lower()
+
     
